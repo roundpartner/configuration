5b5fb0e9cccc3127f8dc0f1b4c33adb9b7e7335c
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-03-20 16:39:11 +0000

    Use command builder for process

--- src/Plugin/RemoteFile.php
+++ src/Plugin/RemoteFile.php
@@ -3,9 +3,13 @@
 namespace RoundPartner\Conf\Plugin;
 
 use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
 
 class RemoteFile implements PluginInterface
 {
+    const REMOTE_CONFIG_PATH = 'alice:/opt/roundpartner/library/rp-conf/configs/';
+    const CONFIG_SUFFIX = '.ini';
+    const COMMAND_PREFIX = 'scp';
 
     /**
      * @param string $config
@@ -15,16 +19,36 @@ class RemoteFile implements PluginInterface
      */
     public function pullConfig($config, $workingDirectory)
     {
-        $command = sprintf('scp alice:/opt/roundpartner/library/rp-conf/configs/%s.ini %s.ini', $config, $config);
-        $process = new Process($command, $workingDirectory);
         if (!is_dir($workingDirectory)) {
             return false;
         }
+        $process = $this->getProcess($config, $workingDirectory);
         $process->run();
         if (!$process->isSuccessful()) {
             return false;
         }
 
         return true;
+    }
+
+    /**
+     * @param string $config
+     * @param string $workingDirectory
+     *
+     * @return Process
+     */
+    public function getProcess($config, $workingDirectory)
+    {
+        $builder = new ProcessBuilder();
+        $arguments = [
+            self::REMOTE_CONFIG_PATH . $config . self::CONFIG_SUFFIX,
+            $config . self::CONFIG_SUFFIX
+        ];
+
+        return $builder
+            ->setWorkingDirectory($workingDirectory)
+            ->setPrefix(self::COMMAND_PREFIX)
+            ->setArguments($arguments)
+            ->getProcess();
     }
 }
--- tests/Unit/Plugin/RemoteFileTest.php
+++ tests/Unit/Plugin/RemoteFileTest.php
@@ -3,10 +3,30 @@
 class RemoteFileTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var string
+     */
+    protected $workingDirectory;
+
+    /**
+     * @var \RoundPartner\Conf\Plugin\RemoteFile
+     */
+    protected $service;
+    
+    public function setUp()
+    {
+        $this->workingDirectory = CONFIG_DIR;
+        $this->service = new \RoundPartner\Conf\Plugin\RemoteFile();
+    }
+
+    public function testGetProcess()
+    {
+        $process = $this->service->getProcess('test', $this->workingDirectory);
+        $this->assertEquals("'scp' 'alice:/opt/roundpartner/library/rp-conf/configs/test.ini' 'test.ini'", $process->getCommandLine());
+    }
+    
     public function testPullConfig()
     {
-        $workingDirectory = CONFIG_DIR;
-        $service = new \RoundPartner\Conf\Plugin\RemoteFile();
-        $this->assertTrue($service->pullConfig('test', $workingDirectory));
+        $this->assertTrue($this->service->pullConfig('test', $this->workingDirectory));
     }
 }
