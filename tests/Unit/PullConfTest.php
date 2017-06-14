5888015be6f46e7ad274d90dc36bdca5d80b6cfc
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-06-15 00:19:44 +0100

    Create test directory

--- tests/Unit/PullConfTest.php
+++ tests/Unit/PullConfTest.php
@@ -9,9 +9,23 @@ class PullConfTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $testDirectory = '/tmp/configs';
+
         $this->cleanConfigFolder();
         $this->config = new \RoundPartner\Conf\PullConf(CONFIG_DIR);
-        $this->config->addPlugin(new \RoundPartner\Conf\Plugin\File(CONFIG_DIR));
+        $this->config->addPlugin(new \RoundPartner\Conf\Plugin\File($testDirectory));
+
+        if (!file_exists($testDirectory)) {
+            mkdir($testDirectory);
+        }
+        $data = <<<DATA
+[test]
+test_key_one='test value two'
+test_key_two='test value two'
+test_key_three='test value three'
+DATA;
+
+        file_put_contents($testDirectory . '/test.ini', $data);
     }
 
     public function testGetConfig()
