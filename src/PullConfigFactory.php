058cd46cd286d76829ceee60b3b49d0f5720854d
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-06-15 01:51:14 +0100

    Enable adding a class or string as a plugin

--- src/PullConfigFactory.php
+++ src/PullConfigFactory.php
@@ -2,23 +2,26 @@
 
 namespace RoundPartner\Conf;
 
-use RoundPartner\Conf\Plugin\File;
-use RoundPartner\Conf\Plugin\RemoteFile;
-
 class PullConfigFactory
 {
     /**
      * Create new instance with all plugins loaded
      *
      * @param string $configDirectory
+     * @param PluginInterface[] $plugins
      *
      * @return PullConf
      */
-    public static function create($configDirectory)
+    public static function create($configDirectory, $plugins = ['RoundPartner\Conf\Plugin\File', 'RoundPartner\Conf\Plugin\RemoteFile'])
     {
         $service = new PullConf($configDirectory);
-        $service->addPlugin(new File());
-        $service->addPlugin(new RemoteFile());
+        foreach ($plugins as $plugin) {
+            if (is_string($plugin)) {
+                $service->addPlugin(new $plugin());
+            } else {
+                $service->addPlugin($plugin);
+            }
+        }
         return $service;
     }
 }
